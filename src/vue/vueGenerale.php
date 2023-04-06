7f657d1c099257890120a5661c916fbfbb4738ab
Romain MILLAN <hello@romainmillan.fr>
2023-04-06 18:19:03 +0200

    Start UI

--- src/vue/vueGenerale.php
+++ src/vue/vueGenerale.php
@@ -2,72 +2,59 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title><?= $pagetitle ?></title>
-    <link rel="stylesheet" href="../ressources/css/navstyle.css">
+    <title>Waps - <?= $pagetitle ?></title>
+    <link rel="stylesheet" href="../ressources/css/main.css">
+    <link rel="stylesheet" href="../ressources/css/bootstrap.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.0/css/line.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css" />
+    <link rel="shortcut icon" href="../ressources/img/icone.svg" type="image/x-icon">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
 </head>
 <body>
     <header class="header">
+        <a href="controleurFrontal.php" class="nav__logo">
+            <img class="logo" src="../ressources/img/logo.png" alt="logo"/>
+        </a>
         <nav class="nav__container">
-            <a href="" class="nav__logo">
-                <img class="logo" src="../ressources/img/logo.png" alt="logo"/>
-            </a>
+            <a class="nav_a" href="controleurFrontal.php">Accueil</a>
+            <a class="nav_a" href="controleurFrontal.php?action=afficherListe&controleur=utilisateur">Utilisateurs</a>
+            <a class="nav_a" href="controleurFrontal.php?action=afficherListe&controleur=noeudCommune">Communes</a>
 
-            <ul>
-                <li>
-                    <a href="controleurFrontal.php?action=afficherListe&controleur=utilisateur">Utilisateurs</a>
-                </li>
-                <li>
-                    <a href="controleurFrontal.php?action=afficherListe&controleur=noeudCommune">Communes</a>
-                </li>
-                <?php
+            <?php
 
-                use App\PlusCourtChemin\Lib\ConnexionUtilisateur;
+            use App\PlusCourtChemin\Lib\ConnexionUtilisateur;
 
-                if (!ConnexionUtilisateur::estConnecte()) {
-                    echo <<<HTML
-                    <li>
-                        <a href="controleurFrontal.php?action=afficherFormulaireConnexion&controleur=utilisateur">
-                            <img alt="login" src="../ressources/img/enter.png" width="18">
-                        </a>
-                    </li>
+            if (!ConnexionUtilisateur::estConnecte()) {
+                echo <<<HTML
+                    <a href="controleurFrontal.php?action=afficherFormulaireConnexion&controleur=utilisateur"><button class="nav_button">Connexion</button></a>
                     HTML;
-                } else {
-                    $loginHTML = htmlspecialchars(ConnexionUtilisateur::getLoginUtilisateurConnecte());
-                    $loginURL = rawurlencode(ConnexionUtilisateur::getLoginUtilisateurConnecte());
-                    echo <<<HTML
-                    <li>
-                        <a href="controleurFrontal.php?action=afficherDetail&controleur=utilisateur&login=$loginURL">
+            } else {
+                $loginHTML = htmlspecialchars(ConnexionUtilisateur::getLoginUtilisateurConnecte());
+                $loginURL = rawurlencode(ConnexionUtilisateur::getLoginUtilisateurConnecte());
+                echo <<<HTML
+                    <a href="controleurFrontal.php?action=deconnecter&controleur=utilisateur"><button class="nav_button">Deconnexion</button></a>
+                    <a href="controleurFrontal.php?action=afficherDetail&controleur=utilisateur&login=$loginURL">
                             <img alt="user" src="../ressources/img/user.png" width="18">
                             $loginHTML
                         </a>
-                    </li>
-                    <li>
-                        <a href="controleurFrontal.php?action=deconnecter&controleur=utilisateur">
-                            <img alt="logout" src="../ressources/img/logout.png" width="18">
-                        </a>
-                    </li>
                     HTML;
-                }
-                ?>
-            </ul>
+            }
+            ?>
         </nav>
-        <div>
-            <?php
-            foreach (["success", "info", "warning", "danger"] as $type) {
-                foreach ($messagesFlash[$type] as $messageFlash) {
-                    echo <<<HTML
+    </header>
+    <div>
+        <?php
+        foreach (["success", "info", "warning", "danger"] as $type) {
+            foreach ($messagesFlash[$type] as $messageFlash) {
+                echo <<<HTML
                     <div class="alert alert-$type">
                         $messageFlash
                     </div>
                     HTML;
-                }
             }
-            ?>
-        </div>
-    </header>
+        }
+        ?>
+    </div>
     <main>
         <?php
         /**
