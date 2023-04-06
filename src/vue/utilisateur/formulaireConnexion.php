08dbc8b964cba15d608f45e3f5e0f780063596b9
Romain MILLAN <hello@romainmillan.fr>
2023-04-06 20:26:10 +0200

    Formulaire Utilisateur

--- src/vue/utilisateur/formulaireConnexion.php
+++ src/vue/utilisateur/formulaireConnexion.php
@@ -1,20 +1,20 @@
-<div>
+<link rel="stylesheet" href="../ressources/css/formUser.css">
+<div class="form-user">
     <form method="<?= $method ?>" action="controleurFrontal.php">
-        <fieldset>
-            <legend>Connexion</legend>
-            <p class="InputAddOn">
-                <label class="InputAddOn-item" for="login_id">Login</label>
-                <input class="InputAddOn-field" type="text" value="" placeholder="Ex : rlebreton" name="login" id="login_id" required>
-            </p>
-            <p class="InputAddOn">
-                <label class="InputAddOn-item" for="mdp_id">Mot de passe</label>
-                <input class="InputAddOn-field" type="password" value="" placeholder="" name="mdp" id="mdp_id" required>
-            </p>
-            <input type='hidden' name='action' value='connecter'>
-            <input type='hidden' name='controleur' value='utilisateur'>
-            <p>
-                <input class="InputAddOn-field" type="submit" value="Envoyer"/>
-            </p>
-        </fieldset>
+        <h2 class="form-user-title">Connexion</h2>
+        <p class="InputUser-div">
+            <label class="InputUser-item" for="login_id">Adresse mail</label>
+            <input class="InputUser-field" type="text" placeholder="dev@waps.fr" name="login" id="login_id" required>
+        </p>
+        <p class="InputUser-div">
+            <label class="InputUser-item" for="mdp_id">Mot de passe</label>
+            <input class="InputUser-field" type="password" placeholder="*************" name="mdp" id="mdp_id" required>
+        </p>
+        <a class="form-user-a-link" href="controleurFrontal.php?action=afficherFormulaireCreation&controleur=utilisateur">S'inscrire</a>
+        <input type='hidden' name='action' value='connecter'>
+        <input type='hidden' name='controleur' value='utilisateur'>
+        <p>
+            <input class="InputUser-submit" type="submit" value="Se connecter"/>
+        </p>
     </form>
 </div>
--- src/vue/utilisateur/formulaireCreation.php
+++ src/vue/utilisateur/formulaireCreation.php
@@ -1,47 +1,46 @@
-<div>
+<link rel="stylesheet" href="../ressources/css/formUser.css">
+<div class="form-user">
     <form method="<?= $method ?>" action="controleurFrontal.php">
-        <fieldset>
-            <legend>Mon formulaire :</legend>
-            <p class="InputAddOn">
-                <label class="InputAddOn-item" for="login_id">Login&#42;</label>
-                <input class="InputAddOn-field" type="text" value="" placeholder="Ex : rlebreton" name="login" id="login_id" required>
-            </p>
-            <p class="InputAddOn">
-                <label class="InputAddOn-item" for="prenom_id">Prenom&#42;</label>
-                <input class="InputAddOn-field" type="text" value="" placeholder="Ex : Romain" name="prenom" id="prenom_id" required>
-            </p>
-            <p class="InputAddOn">
-                <label class="InputAddOn-item" for="nom_id">Nom&#42;</label>
-                <input class="InputAddOn-field" type="text" value="" placeholder="Ex : Lebreton" name="nom" id="nom_id" required>
-            </p>
-            <p class="InputAddOn">
-                <label class="InputAddOn-item" for="email_id">Email&#42;</label>
-                <input class="InputAddOn-field" type="email" value="" placeholder="rlebreton@yopmail.com" name="email" id="email_id" required>
-            </p>
-            <p class="InputAddOn">
-                <label class="InputAddOn-item" for="mdp_id">Mot de passe&#42;</label>
-                <input class="InputAddOn-field" type="password" value="" placeholder="" name="mdp" id="mdp_id" required>
-            </p>
-            <p class="InputAddOn">
-                <label class="InputAddOn-item" for="mdp2_id">Vérification du mot de passe&#42;</label>
-                <input class="InputAddOn-field" type="password" value="" placeholder="" name="mdp2" id="mdp2_id" required>
-            </p>
-            <?php
+        <h2 class="form-user-title">Mon formulaire :</h2>
+        <p class="InputUser-div">
+            <label class="InputUser-item" for="login_id">Login&#42;</label>
+            <input class="InputUser-field" type="text" value="" placeholder="dev@waps.fr" name="login" id="login_id" required>
+        </p>
+        <p class="InputUser-div">
+            <label class="InputUser-item" for="email_id">Email&#42;</label>
+            <input class="InputUser-field" type="email" value="" placeholder="rlebreton@yopmail.com" name="email" id="email_id" required>
+        </p>
+        <p class="InputUser-div">
+            <label class="InputUser-item" for="prenom_id">Prenom&#42;</label>
+            <input class="InputUser-field" type="text" value="" placeholder="Romain" name="prenom" id="prenom_id" required>
+        </p>
+        <p class="InputUser-div">
+            <label class="InputUser-item" for="nom_id">Nom&#42;</label>
+            <input class="InputUser-field" type="text" value="" placeholder="Lebreton" name="nom" id="nom_id" required>
+        </p>
+        <p class="InputUser-div">
+            <label class="InputUser-item" for="mdp_id">Mot de passe&#42;</label>
+            <input class="InputUser-field" type="password" value="" placeholder="************" name="mdp" id="mdp_id" required>
+        </p>
+        <p class="InputUser-div">
+            <label class="InputUser-item" for="mdp2_id">Vérification du mot de passe&#42;</label>
+            <input class="InputUser-field" type="password" value="" placeholder="************" name="mdp2" id="mdp2_id" required>
+        </p>
+        <?php
 
-            use App\PlusCourtChemin\Lib\ConnexionUtilisateur;
+        use App\PlusCourtChemin\Lib\ConnexionUtilisateur;
 
-            if (ConnexionUtilisateur::estAdministrateur()) {
+        if (ConnexionUtilisateur::estAdministrateur()) {
             ?>
-                <p class="InputAddOn">
-                    <label class="InputAddOn-item" for="estAdmin_id">Administrateur</label>
-                    <input class="InputAddOn-field" type="checkbox" placeholder="" name="estAdmin" id="estAdmin_id">
-                </p>
-            <?php } ?>
-            <input type='hidden' name='action' value='creerDepuisFormulaire'>
-            <input type='hidden' name='controleur' value='utilisateur'>
-            <p class="InputAddOn">
-                <input class="InputAddOn-field" type="submit" value="Envoyer" />
+            <p class="InputUser-div">
+                <label class="InputUser-item" for="estAdmin_id">Administrateur</label>
+                <input class="InputUser-field" type="checkbox" placeholder="" name="estAdmin" id="estAdmin_id">
             </p>
-        </fieldset>
+        <?php } ?>
+
+        <input type='hidden' name='action' value='creerDepuisFormulaire'>
+        <input type='hidden' name='controleur' value='utilisateur'>
+
+        <input class="InputUser-submit" type="submit" value="Crée son compte" />
     </form>
 </div>
