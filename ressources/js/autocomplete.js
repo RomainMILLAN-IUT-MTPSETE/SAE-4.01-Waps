18fd9a5104533ba1a2bcf357d9401c2f76cf0b7d
geoff <geoffreypierre2001@gmail.com>
2023-04-07 20:24:21 +0200

    jen ai mar

--- ressources/js/autocomplete.js
+++ ressources/js/autocomplete.js
@@ -1,122 +1,170 @@
-const divAutocompletionDepart = document.getElementById(`autoCompletionDepart`);
 const divAutocompletionArrivee = document.getElementById(`autoCompletionArrivee`);
-const inputVilleDepart = document.getElementById(`nomCommuneDepart_id`);
+const divAutocompletionDepart = document.getElementById(`autoCompletionDepart`);
 const inputVilleArrivee = document.getElementById(`nomCommuneArrivee_id`);
+const inputVilleDepart = document.getElementById(`nomCommuneDepart_id`);
+let communesSelectionneesDepart = [];
+let communesSelectionneesArrivee = [];
 let communes = [];
+
 
 let xhr = new XMLHttpRequest();
 xhr.open(`GET`, `controleurFrontal.php?controleur=noeudCommune&action=getNomsCommunesJSON`, true);
-xhr.onload = function(){
-    if(xhr.status === 200){
+xhr.onload = function () {
+    if (xhr.status === 200) {
         callback(xhr);
     }
 };
 xhr.send(null);
 
-function callback(xhr){
+function callback(xhr) {
     communes = Array.from(JSON.parse(xhr.responseText));
+
     inputVilleArrivee.addEventListener(`input`, (e) => {
         const value = e.target.value;
+        communesSelectionneesArrivee = Array.from(communes).filter((commune) => {
+            return commune.toLowerCase().startsWith(value.toLowerCase());
+        }).slice(0, 5);
         videVilles(divAutocompletionArrivee);
-        let communesSelectionnees = Array.from(communes).filter((commune) => {
-            return commune.toLowerCase().startsWith(value.toLowerCase());
-        }).slice(0, 5);
-        if(navigator.geolocation){
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((pos) => {
                 fetch(`https://api-adresse.data.gouv.fr/reverse/?lon=${pos.coords.longitude}&lat=${pos.coords.latitude}`)
                     .then(response => response.json())
                     .then(data => {
-                        if(communes.includes(data.features[0].properties.city)){
+                        if (communes.includes(data.features[0].properties.city)) {
                             city = data.features[0].properties.city;
-                            communesSelectionnees = ["Votre position"].concat(communesSelectionnees);
-                            afficheVilles(communesSelectionnees, divAutocompletionArrivee);
-                        }
-                    })
-                    .catch(error => console.log(error));
-            });
-        }else{
-            afficheVilles(communesSelectionnees, divAutocompletionArrivee);
-        }
-    });
-    inputVilleDepart.addEventListener(`input`, (e) => {
-        const value = e.target.value;
-        videVilles(divAutocompletionDepart);
-        let communesSelectionnees = Array.from(communes).filter((commune) => {
-            return commune.toLowerCase().startsWith(value.toLowerCase());
-        }).slice(0, 5);
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition((pos) => {
-                fetch(`https://api-adresse.data.gouv.fr/reverse/?lon=${pos.coords.longitude}&lat=${pos.coords.latitude}`)
-                    .then(response => response.json())
-                    .then(data => {
-                        if(communes.includes(data.features[0].properties.city)){
-                            city = data.features[0].properties.city;
-                            communesSelectionnees = ["Votre position"].concat(communesSelectionnees);
-                            if (inputVilleDepart.value.length >= 2) {
-                                afficheVilles(communesSelectionnees, divAutocompletionDepart);
+                            communesSelectionneesArrivee = ["Votre position"].concat(communesSelectionneesArrivee);
+                            if (inputVilleArrivee.value.length >= 2) {
+                                afficheVilles(communesSelectionneesArrivee, divAutocompletionArrivee);
                             }
                         }
                     })
                     .catch(error => console.log(error));
             });
-        }else{
+        } else {
+            if (inputVilleArrivee.value.length >= 2) {
+                afficheVilles(communesSelectionneesArrivee, divAutocompletionArrivee);
+            }
+        }
+    });
+
+
+    inputVilleDepart.addEventListener(`input`, (e) => {
+        const value = e.target.value;
+        communesSelectionneesDepart = Array.from(communes).filter((commune) => {
+            return commune.toLowerCase().startsWith(value.toLowerCase());
+        }).slice(0, 5);
+        videVilles(divAutocompletionDepart);
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((pos) => {
+                fetch(`https://api-adresse.data.gouv.fr/reverse/?lon=${pos.coords.longitude}&lat=${pos.coords.latitude}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        if (communes.includes(data.features[0].properties.city)) {
+                            city = data.features[0].properties.city;
+                            communesSelectionneesDepart = ["Votre position"].concat(communesSelectionneesDepart);
+                            if (inputVilleDepart.value.length >= 2) {
+                                afficheVilles(communesSelectionneesDepart, divAutocompletionDepart);
+                            }
+                        }
+                    })
+                    .catch(error => console.log(error));
+            });
+        } else {
             if (inputVilleDepart.value.length >= 2) {
-                afficheVilles(communesSelectionnees, divAutocompletionDepart);
+                afficheVilles(communesSelectionneesDepart, divAutocompletionDepart);
             }
         }
     });
 }
 
-function videVilles(target){
-    while (target.hasChildNodes()) target.removeChild(target.firstChild);
+inputVilleDepart.onkeydown = event => {
+    if (event.code === 'ArrowUp') {
+        if (0 <= communesSelectionneesDepart.indexOf(inputVilleDepart.value) - 1) {
+            inputVilleDepart.value = communesSelectionneesDepart[communesSelectionneesDepart.indexOf(inputVilleDepart.value) - 1]
+        } else {
+            inputVilleDepart.value = communesSelectionneesDepart[communesSelectionneesDepart.length - 1]
+        }
+    } else if (event.code === 'ArrowDown') {
+        if (communesSelectionneesDepart.indexOf(inputVilleDepart.value) + 1 < communesSelectionneesDepart.length) {
+            inputVilleDepart.value = communesSelectionneesDepart[communesSelectionneesDepart.indexOf(inputVilleDepart.value) + 1]
+        } else {
+            inputVilleDepart.value = communesSelectionneesDepart[0]
+        }
+    } else if (event.code === 'Enter') {
+        videVilles(divAutocompletionDepart)
+    }
+    Array.from(divAutocompletionDepart.children).forEach(element => {
+        if (element.innerText === inputVilleDepart.value) {
+            element.style.backgroundColor = "grey"
+        } else {
+            element.style.backgroundColor = ""
+        }
+    })
 }
 
-function afficheVilles(villes, target){
+inputVilleArrivee.onkeydown = event => {
+    if (event.code === 'ArrowUp') {
+        if (0 <= communesSelectionneesArrivee.indexOf(inputVilleArrivee.value) - 1) {
+            inputVilleArrivee.value = communesSelectionneesArrivee[communesSelectionneesArrivee.indexOf(inputVilleArrivee.value) - 1]
+        } else {
+            inputVilleArrivee.value = communesSelectionneesArrivee[communesSelectionneesArrivee.length - 1]
+        }
+    } else if (event.code === 'ArrowDown') {
+        if (communesSelectionneesArrivee.indexOf(inputVilleArrivee.value) + 1 < communesSelectionneesArrivee.length) {
+            inputVilleArrivee.value = communesSelectionneesArrivee[communesSelectionneesArrivee.indexOf(inputVilleArrivee.value) + 1]
+        } else {
+            inputVilleArrivee.value = communesSelectionneesArrivee[0]
+        }
+    } else if (event.code === 'Enter') {
+        videVilles(divAutocompletionArrivee)
+    }
+    Array.from(divAutocompletionArrivee.children).forEach(element => {
+        if (element.innerText === inputVilleArrivee.value) {
+            element.style.backgroundColor = "grey"
+        } else {
+            element.style.backgroundColor = ""
+        }
+    })
+}
+
+function videVilles(target) {
+    target.innerHTML = "";
+}
+
+function afficheVilles(villes, target) {
+    videVilles(target);
     for (let i = 0; i < villes.length; i++) {
-        let temp = document.createElement(`p`);
-        temp.setAttribute(`id`, `champ-ville-${i}`);
-        temp.innerHTML = villes[i];
-        target.appendChild(temp);
+        let p = document.createElement(`p`);
+        p.id  = `champ-ville-${i}`;
+        p.innerText = villes[i];
+        target.appendChild(p);
     }
 }
 
 divAutocompletionDepart.addEventListener('click', event => {
     inputVilleDepart.value = event.target.innerHTML;
-    videVilles();
+    videVilles(divAutocompletionDepart);
 });
 
-inputVilleDepart.onkeydown = event => {
-    if (event.code === 'ArrowUp') {
-        if (0 <= communes.indexOf(inputVilleDepart.value) - 1) {
-            inputVilleDepart.value = communes[communes.indexOf(inputVilleDepart.value) - 1]
-        } else {
-            inputVilleDepart.value = communes[communes.length - 1]
-        }
-    } else if (event.code === 'ArrowDown') {
-        if (communes.indexOf(inputVilleDepart.value) + 1 < communes.length) {
-            inputVilleDepart.value = communes[communes.indexOf(inputVilleDepart.value) + 1]
-        } else {
-            inputVilleDepart.value = communes[0]
-        }
-    } else if (event.code === 'Enter') {
-        videVilles()
-    }
-    Array.from(divAutocompletionDepart.children).forEach(element => {
-        if (element.innerHTML === inputVilleDepart.value) {
-            element.style.backgroundColor = "black"
-            element.style.color = "white";
-        } else {
-            element.style.backgroundColor = ""
-            element.style.color = "black";
-        }
-    })
-}
+divAutocompletionArrivee.addEventListener('click', event => {
+    inputVilleArrivee.value = event.target.innerHTML;
+    videVilles(divAutocompletionArrivee);
+});
 
+inputVilleArrivee.addEventListener('click', event => {
+    inputVilleArrivee.value = event.target.innerHTML;
+    videVilles(divAutocompletionDepart);
+});
+
+inputVilleDepart.addEventListener('click', event => {
+    inputVilleDepart.value = event.target.innerHTML;
+    videVilles(divAutocompletionArrivee);
+});
 
 /**
  * A faire:
  * - Afficher villes que si la longueur de la value des inputs >= 2
- * - Remplissage de l'input quand l'utilisateur clique sur le champ 
+ * - Remplissage de l'input quand l'utilisateur clique sur le champ
  * - Sélection du champ avec les flèches du clavier (faire avec les keycode === numéroDeLaTouche et pas keycode = 'ArrowDown' ou 'ArrowUp);
  * - La valeur de l'input prend la ville actuelle de l'utilisateur (stockée dans city) lors du clic sur le champ "Votre position" de la div autoCompletion
  */
--- src/vue/noeudCommune/plusCourtChemin.php
+++ src/vue/noeudCommune/plusCourtChemin.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="../ressources/css/plusCourtChemin.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css"/>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
 <script type="text/javascript" src="../ressources/js/autocomplete.js" defer></script>
 <script type="text/javascript" src="../ressources/js/map.js" defer></script>
@@ -8,19 +8,23 @@
         <h2 class="choose_title">Choisissez votre itinéraire !</h2>
         <form action="" method="post" autocomplete="off">
             <div class="inputDiv">
-                  <p class="Input-label" for="nomCommuneDepart_id">Point de départ</p>
-                <div class="ConteneurAutoCompletion">
-                <input class="Input-field" type="text" value="" placeholder="Ex : Sète" name="nomCommuneDepart" id="nomCommuneDepart_id" required>
-                <div class="autoCompletionDepart" id="autoCompletionDepart"></div>
+                <p class="Input-label" for="nomCommuneDepart_id">Point de départ</p>
+                <div class="ConteneurAutoCompletionDepart">
+                    <input class="Input-field" type="text" value="" placeholder="Ex : Sète" name="nomCommuneDepart"
+                           id="nomCommuneDepart_id" required>
+                    <div class="autoCompletionDepart" id="autoCompletionDepart"></div>
+                </div>
             </div>
-        </div>
             <div class="inputDiv">
                 <p class="Input-label" for="nomCommuneArrivee_id">Point d’arrivée</p>
-                <input class="Input-field" type="text" value="" placeholder="Ex : Montpellier" name="nomCommuneArrivee" id="nomCommuneArrivee_id" required>
+                <div class="ConteneurAutoCompletionArrivee">
+                    <input class="Input-field" type="text" value="" placeholder="Ex : Montpellier"
+                           name="nomCommuneArrivee" id="nomCommuneArrivee_id" required>
+                    <div class="autoCompletionArrivee" id="autoCompletionArrivee"></div>
+                </div>
             </div>
-            <div class="autoCompletionArrivee" id="autoCompletionArrivee"></div>
             <!-- <input type="hidden" name="XDEBUG_TRIGGER"> -->
-            <input class="Input-submit" type="submit" value="Calculer" />
+            <input class="Input-submit" type="submit" value="Calculer"/>
         </form>
     </div>
     <div class="pcc-resultat">
@@ -28,7 +32,8 @@
         <div class="resultat">
             <?php if (!empty($_POST)) { ?>
                 <p>
-                    Le trajet entre <?= $nomCommuneDepart ?> et <?= $nomCommuneArrivee ?> fait <?= $distance ?>km. <?php if(App\PlusCourtChemin\Modele\HTTP\Cookie::existeCle("temps_calcul")) echo("<br/>Le temps de calcul à pris " . App\PlusCourtChemin\Modele\HTTP\Cookie::lire("temps_calcul") . " secondes.") ?>
+                    Le trajet entre <?= $nomCommuneDepart ?> et <?= $nomCommuneArrivee ?> fait <?= $distance ?>
+                    km. <?php if (App\PlusCourtChemin\Modele\HTTP\Cookie::existeCle("temps_calcul")) echo("<br/>Le temps de calcul à pris " . App\PlusCourtChemin\Modele\HTTP\Cookie::lire("temps_calcul") . " secondes.") ?>
                 </p>
             <?php } ?>
         </div>
